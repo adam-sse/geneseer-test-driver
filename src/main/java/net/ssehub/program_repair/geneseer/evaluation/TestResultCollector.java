6b9264dcb1315f75e41b68b5f1c669dd2f4998c3
Adam <adam@sse.uni-hildesheim.de>
2023-06-07 16:25:13 +0200

    Actually ignore test methods that don't exist, as stated in readme

--- src/main/java/net/ssehub/program_repair/geneseer/evaluation/TestResultCollector.java
+++ src/main/java/net/ssehub/program_repair/geneseer/evaluation/TestResultCollector.java
@@ -34,8 +34,17 @@ public class TestResultCollector extends RunListener {
     public void testFailure(Failure failure) {
         TestResult testResult = this.executedTests.get(failure.getDescription());
         if (testResult != null) {
-            testResult.setFailureMessage(failure.getMessage());
-            testResult.setFailureStacktrace(failure.getTrimmedTrace());
+            
+            if (failure.getDescription().getClassName().equals("org.junit.runner.manipulation.Filter")
+                    && failure.getMessage().contains("No tests found matching Method")) {
+                // this "failure" is created when a non-existing test method name is requested; we don't want it
+                this.executedTests.remove(failure.getDescription());
+                
+            } else {
+                testResult.setFailureMessage(failure.getMessage());
+                testResult.setFailureStacktrace(failure.getTrimmedTrace());
+            }
+            
         } else {
             throw new RuntimeException("Test failed that wasn't started");
         }
